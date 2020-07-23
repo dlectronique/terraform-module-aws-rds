694e9b2ca3a38b4abd712218d857e991be312601
dlectronique <dominique@hashicorp.com>
2020-07-23 14:08:30 +0100

    removed quotes main.tf vars

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_db_instance" "domi-demo" {
   allocated_storage    = 20
-  storage_type         = "var.db_storage_type"
-  engine               = "var.db_engine"
-  engine_version       = "var.db_engine_version"
-  instance_class       = "var.db_instance_class"
-  name                 = "var.db_name"
-  username             = "var.db_username"
-  password             = "var.db_password"
-  parameter_group_name = "var.db_parameter_group_name"
+  storage_type         = var.db_storage_type
+  engine               = var.db_engine
+  engine_version       = var.db_engine_version
+  instance_class       = var.db_instance_class
+  name                 = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = var.db_parameter_group_name
 }
 
