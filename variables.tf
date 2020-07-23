a19644cb93c5cf4e7f6c6e67bb2a4563ee9a8f87
dlectronique <dominique@hashicorp.com>
2020-07-23 11:59:48 +0100

    moved around some stuff, see if this works

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+### a list of variables
+
 variable "region" {
 	type = "string"
 	description = "The AWS region where you're going to deploy"
@@ -11,41 +13,41 @@ variable "db_allocated_storage" {
 }
 
 variable "db_storage_type" {
-	description "What type of AWS storage you are using"
-	default "gp2"
+	description = "What type of AWS storage you are using"
+	default = "gp2"
 }
 
-variable "db_engine"{
-	description "What kind of DB Engine you are using"
-	default "mysql"
+variable "db_engine" {
+	description = "What kind of DB Engine you are using"
+	default = "mysql"
 }
 
-variable "db_engine_version"{
-	description "What version of the DB engine you are using"
-	default
+variable "db_engine_version" {
+	description = "What version of the DB engine you are using"
+	default = "5.7"
 }
 
-variable "db_instance_class"{
-	description "Which DB instance class are you deploying on"
-	default "db.t2.micro"
+variable "db_instance_class" {
+	description = "Which DB instance class are you deploying on"
+	default = "db.t2.micro"
 }
 
-variable "db_name"{
-	description "Name of your database"
-	default "domi-db"
+variable = "db_name" {
+	description = "Name of your database"
+	default = "domidb"
 }
 
-variable "db_username"{
-	description "What is your username?"
-	default "dlectronique"
+variable "db_username" {
+	description = "What is your username?"
+	default = "dlectronique"
+}
+ 
+variable "db_password" {
+	description = "Password"
 }
 
-variable "db_password"{
-	description "Password"
-}
-
-variable "db_parameter_group_name"{
-	description "Parameter group name for the db"
-	default "default.mysql5.7"
+variable "db_parameter_group_name" {
+	description = "Parameter group name for the db"
+	default = "default.mysql5.7"
 }
 
